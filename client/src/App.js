4a71a3b0a8e59806d2ac6141a880aec4a00027b5
Yehui <yehuizhang@outlook.com>
2020-01-06 12:52:09 -0800

    :hammer: Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,18 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import store from './redux/store';
+import { Home, CreateUser, EditUser } from './components';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router></Router>
+      <Router>
+        <Switch>
+          <Route exact path="/create-user" component={CreateUser} />
+          <Route exact path="/edit-user/:id" component={EditUser} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </Provider>
   );
 }
---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+import Home from './Home';
+import CreateUser from './CreateUser';
+import EditUser from './EditUser';
+
+export { Home, CreateUser, EditUser };
