d39d0dda0e979b0b068b6a67a2c0b98ce215a953
Yehui <yehuizhang@outlook.com>
2020-01-08 22:15:14 -0800

    Added LoadOfficer (Redux) - Minor fix

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux';
 
 import feedback from './feedback';
 import notifier from './notifier';
+import officers from './officers';
 
-export default combineReducers({ feedback, notifier });
+export default combineReducers({ feedback, notifier, officers });
