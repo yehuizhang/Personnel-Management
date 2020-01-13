80862a5e2cfa80b1b92b2c2bd4ff4ebdcc0a2ee5
Yehui <yehuizhang@outlook.com>
2020-01-13 02:55:14 -0800

    :wrench: Fix bug on superior dropdown
    * Superior's value could be undefined if officers were not loaded.

--- client/src/redux/actions/userActions/editUser.js
+++ client/src/redux/actions/userActions/editUser.js
@@ -4,7 +4,7 @@ import { RESET_CURRENT_USER } from '../../types';
 import { axiosJSONConfig } from '../config';
 import { setAlert } from '../notifier';
 import { unsetLoading, setLoading } from '../spinner';
-import { filterUserData } from '../user';
+import { filterUserData, resetCurrentUser } from '../user';
 import { reloadUserList } from '../userList';
 
 const updateUser = (userData, history) => async dispatch => {
@@ -15,9 +15,7 @@ const updateUser = (userData, history) => async dispatch => {
     await axios.put('/api/user/', userData, axiosJSONConfig);
     setAlert('User successfully updated!', 'success')(dispatch);
     reloadUserList()(dispatch);
-    dispatch({
-      type: RESET_CURRENT_USER,
-    });
+    resetCurrentUser()(dispatch);
     unsetLoading()(dispatch);
     history.push('/');
   } catch (error) {
