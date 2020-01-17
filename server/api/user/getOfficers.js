77175a62545ffd974a0c85a2e8d4361a297a208a
Yehui <yehuizhang@outlook.com>
2020-01-16 22:09:51 -0800

    :wrench: Server: Improve getOfficers performance by reducing execution time by ~70%
    prev:
    Get officer time: 5.931ms
    
    curr:
    Get officer time: 2.541ms

--- server/api/user/getOfficers.js
+++ server/api/user/getOfficers.js
@@ -2,15 +2,22 @@ const User = require('../../models/users');
 const { rankToNumber } = require('./util');
 
 const getOfficers = res => {
-  User.find({ rank: { $ne: 'Private' } }, 'name rank', (err, docs) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Can't get officers now. Internal error");
+  console.time('Get officer time');
+  User.find(
+    { rank: { $ne: 'Private' } },
+    'name rank',
+    { lean: true },
+    (err, docs) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Can't get officers now. Internal error");
+      }
+      docs.sort((a, b) => rankToNumber[a.rank] - rankToNumber[b.rank]);
+      docs = docs.map(doc => ({ id: doc._id, rank: doc.rank, name: doc.name }));
+      console.timeEnd('Get officer time');
+      return res.json({ officers: docs });
     }
-    docs.sort((a, b) => rankToNumber[a.rank] - rankToNumber[b.rank]);
-    docs = docs.map(doc => ({ id: doc._id, rank: doc.rank, name: doc.name }));
-    return res.json({ officers: docs });
-  });
+  );
 };
 
 module.exports = getOfficers;
